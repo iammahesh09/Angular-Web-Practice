c93140124528082b39b19e29936a866469385a34
iammahesh09 <iammahesh.nlr@gmail.com>
2018-09-05 09:32:43 +0530

    active routerLink

--- src/app/Forms/forms.component.ts
+++ src/app/Forms/forms.component.ts
@@ -6,15 +6,23 @@ import { Component, OnInit } from '@angular/core';
         <h2 class="text-center display-3">Forms</h2>
         <div class="row">
             <div class="col">
-                <a class="btn btn-md btn-primary btn-block text-white" routerLink="ReactiveForms" routerLinkActive="active">Reactive Form validation</a>
+                <a class="btn btn-md btn-dark btn-block text-white" routerLink="ReactiveForms" routerLinkActive="active">Reactive Form validation</a>
             </div>        
             <div class="col">
-                <a class="btn btn-md btn-info btn-block text-white" routerLink="BasicForms" routerLinkActive="active">Form validation</a>
+                <a class="btn btn-md btn-dark btn-block text-white" routerLink="BasicForms" routerLinkActive="active">Basic Form validation</a>
             </div>
         </div>
         <router-outlet></router-outlet>
 
-        `
+        `,
+    styles: [
+        `.active{  
+            color: #fff !important;
+            background-color: #007bff !important;
+            border-color: #007bff !important;
+            box-shadow:none !important;
+        }`
+    ]
 })
 export class FormsComponent implements OnInit {
 
