56d09c78bcd9990eee12caf01950cb0aec1c443d
iammahesh09 <iammahesh.nlr@gmail.com>
2018-09-26 19:26:57 +0530

    fix bug

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model'
 import { AuthService } from './auth.service';
-import { ResponseInfo } from '../models/response.model';
 
 @Injectable({
   providedIn: 'root'
